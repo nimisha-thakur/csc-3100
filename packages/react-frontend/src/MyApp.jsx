0e54b03beb175b2e0cd9b8c3364f716ed850828d
nimisha-thakur <nithakur@calpoly.edu>
2026-09-14 20:36:48 -0700

    step 3 done

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -31,6 +31,18 @@ function MyApp() {
   return promise;
 }
 
+  function postUser(person) {
+  const promise = fetch("Http://localhost:8000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(person),
+  });
+
+  return promise;
+}
+
   useEffect(() => {
   fetchUsers()
     .then((res) => res.json())
@@ -50,7 +62,11 @@ function removeOneCharacter(index) {
 }
 
 function updateList(person) {
-  setCharacters([...characters, person]);
+  postUser(person)
+    .then(() => setCharacters([...characters, person]))
+    .catch((error) => {
+      console.log(error);
+    });
 }
 
  return (
